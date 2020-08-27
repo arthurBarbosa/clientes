85cef0f41398d8ff1533e9aebedc08b6b91437b9
Arthur Barbosa <arthurbruno03@gmail.com>
2020-08-26 23:05:51 -0400

    [create] criacao do método cadastrar cliente

---
+++ src/main/java/br/com/abcode/clientes/controller/ClienteController.java
@@ -0,0 +1,25 @@
+package br.com.abcode.clientes.controller;
+
+import br.com.abcode.clientes.entity.Cliente;
+import br.com.abcode.clientes.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private final ClienteRepository repository;
+
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+}
