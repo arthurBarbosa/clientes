7ae6bc17f4a250f44021924ab103c19ab24534af
Arthur Barbosa <arthurbruno03@gmail.com>
2020-08-30 09:18:15 -0400

    validação bean validation em  serviços prestados

--- src/main/java/br/com/abcode/clientes/controller/dto/ServicoDTO.java
+++ src/main/java/br/com/abcode/clientes/controller/dto/ServicoDTO.java
@@ -3,11 +3,23 @@ package br.com.abcode.clientes.controller.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoDTO {
+
+    @NotBlank(message = "{campo.descricao.obrigatorio}")
     private  String descricao;
+
+    @NotBlank(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotBlank(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer clienteId;
 }
