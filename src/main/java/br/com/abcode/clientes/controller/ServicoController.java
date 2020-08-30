a93d724701faf777cdbbd7bbca558e0c1cf5fd55
Arthur Barbosa <arthurbruno03@gmail.com>
2020-08-29 23:32:10 -0400

    criacao de pesquisa de servico prestado

--- src/main/java/br/com/abcode/clientes/controller/ServicoController.java
+++ src/main/java/br/com/abcode/clientes/controller/ServicoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/servicos-prestados")
@@ -41,4 +42,14 @@ public class ServicoController {
 
         return repository.save(servico);
     }
+
+    @GetMapping
+    public List<Servico> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) Integer mes
+    ) {
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
+    }
+
+
 }
--- src/main/java/br/com/abcode/clientes/repository/ServicoRepository.java
+++ src/main/java/br/com/abcode/clientes/repository/ServicoRepository.java
@@ -1,10 +1,20 @@
 package br.com.abcode.clientes.repository;
 
-import br.com.abcode.clientes.entity.Cliente;
 import br.com.abcode.clientes.entity.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
+    @Query(" SELECT s FROM Servico s join s.cliente c " +
+            "WHERE UPPER(c.nome) LIKE UPPER (:nome) " +
+            "AND MONTH(s.data) = :mes")
+    List<Servico> findByNomeClienteAndMes(
+            @Param("nome") String nome,
+            @Param("mes") Integer mes);
 }
