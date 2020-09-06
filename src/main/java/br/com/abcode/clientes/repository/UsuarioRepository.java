dba8f263403bdcbe355f1697aafce017b86c8d49
Arthur Barbosa <arthurbruno03@gmail.com>
2020-09-06 12:01:39 -0400

    adicionado service de usuario e configurações na classe SecuritConfig

--- src/main/java/br/com/abcode/clientes/repository/UsuarioRepository.java
+++ src/main/java/br/com/abcode/clientes/repository/UsuarioRepository.java
@@ -4,6 +4,12 @@ import br.com.abcode.clientes.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
