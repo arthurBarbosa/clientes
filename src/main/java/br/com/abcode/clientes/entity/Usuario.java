3a4a87945c9e22172fa97660683f327374a2f41f
Arthur Barbosa <arthurbruno03@gmail.com>
2020-09-06 10:34:57 -0400

    criação do endpoint para salvar usuário

--- src/main/java/br/com/abcode/clientes/entity/Usuario.java
+++ src/main/java/br/com/abcode/clientes/entity/Usuario.java
@@ -1,8 +1,13 @@
 package br.com.abcode.clientes.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Usuario {
 
     @Id
